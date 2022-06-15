df74228b49ca47ec688db351e9937b0828e12aa0
Ishan saxena <61831401+hackishanhack@users.noreply.github.com>
2022-06-15 20:02:21 +0530

    Update gcrDailyReport.py

--- gcrDailyReport.py
+++ gcrDailyReport.py
@@ -4,7 +4,7 @@ import pandas as pd
 from plotly import graph_objs as go
 
 #Variable Names
-date = "14-06" # SET Date as Name - "DD-MM" to the CSV file
+date = "15-06" # SET Date as Name - "DD-MM" to the CSV file
 
 #Program Variables
 header = st.container()
